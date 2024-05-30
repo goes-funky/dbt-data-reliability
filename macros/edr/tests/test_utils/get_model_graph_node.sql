72f5b58ac5872130055f1a9bd69cdc98b1c846b6
konstantin-baidin-y42 <konstantin.baidin@y42.com>
2024-05-30 10:56:04 +0200

    print graph

--- macros/edr/tests/test_utils/get_model_graph_node.sql
+++ macros/edr/tests/test_utils/get_model_graph_node.sql
@@ -8,6 +8,7 @@
         {# model relation is the relation object of the model where the test is defined #}
         {% set relation_name = model_relation.name | lower %}
         {% set depends_on_nodes = elementary.get_nodes_by_unique_ids(test_depends_on_unique_ids) %}
+        {% do elementary.edr_log('PRINT_GRAPH') %}
         {% do elementary.edr_log('depends_on_nodes') %}
         {% do elementary.edr_log(depends_on_nodes) %}
         {% if depends_on_nodes %}
